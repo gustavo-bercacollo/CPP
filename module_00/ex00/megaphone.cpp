ab0a310d4b719d8c43b79e3083b4ff326308f451
gustavo <gustavovilela802@gmail.com>
2026-02-02 17:12:39 -0300

    add ex00

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+void megafone(std::string str)
+{
+  for (size_t i = 0; i < str.length(); i++)
+    std::cout << (char)toupper(str[i]);
+}
+
+int main(int argc, char **argv)
+{
+  if (argc > 1)
+  {
+    for (int i = 1; i < argc; i++)
+      megafone(argv[i]);
+    std::cout << std::endl;
+  }
+  else
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+};
