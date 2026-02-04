b34c43d3da68003e01af119684528ca38c1247d6
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2026-02-04 17:39:36 -0300

    ok

---
+++ module_00/ex01/Contact.hpp
@@ -0,0 +1,11 @@
+#include <string>
+
+class Contact
+{
+  private:
+    std::string firstName;
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+};
