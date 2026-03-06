5a0cf5a2327749d9dac7e82f2629c3df60a8d55c
gustavo <gustavovilela802@gmail.com>
2026-03-05 23:17:39 -0300

    fix bsp

--- module_02/ex03/bsp.cpp
+++ module_02/ex03/bsp.cpp
@@ -1,25 +1,26 @@
 #include "Point.hpp"
 
+Fixed abs_fixed(Fixed value)
+{
+    return (value < 0) ? value * -1 : value;
+}
+
 static Fixed area(Point const a, Point const b, Point const c)
 {
-  return ( (b.getX() - a.getX()) * (c.getY() - a.getY())
-          - (b.getY() - a.getY()) * (c.getX() - a.getX()) );
+  return ((((a.getX() * b.getY()) + (b.getX() * c.getY()) + (c.getX() * a.getY())) - 
+            ((a.getX() * c.getY()) + (c.getX() * b.getY()) + (b.getX() * a.getY())))/2);
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-    Fixed totalArea = area(a, b, c);
+    Fixed totalArea = abs_fixed(area(a, b, c));
 
-    Fixed area1 = area(point, a, b);
-    Fixed area2 = area(point, b, c);
-    Fixed area3 = area(point, c, a);
+    Fixed area1 = abs_fixed(area(point, a, b));
+    Fixed area2 = abs_fixed(area(point, b, c));
+    Fixed area3 = abs_fixed(area(point, c, a));
 
     if (area1 == 0 || area2 == 0 || area3 == 0)
         return false;
 
-    if ((area1 > 0 && area2 > 0 && area3 > 0) ||
-        (area1 < 0 && area2 < 0 && area3 < 0))
-        return true;
-
-    return false;
+    return totalArea == area1 + area2 + area3;
 }
